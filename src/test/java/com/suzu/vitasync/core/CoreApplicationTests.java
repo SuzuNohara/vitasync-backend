364a23baa2fc976fa589180a0c5583cb63590bb2
Etnan Lopez <etnan.lopez@maxikash.com>
2025-06-07 22:49:40 -0600

    initial commit

---
+++ src/test/java/com/suzu/vitasync/core/CoreApplicationTests.java
@@ -0,0 +1,13 @@
+package com.suzu.vitasync.core;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
